d0b8fe839d0c6588f5e6cf1dd108b0ef45a54c14
Sourav Hazra <souravhazra118@gmail.com>
2025-08-13 16:40:49 +0530

    Done

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
   try {
-    let token = req.header("Authorization");
+    let token = req.header("Authorization")
 
     if (!token) {
       return res.status(403).send("Access Denied");
